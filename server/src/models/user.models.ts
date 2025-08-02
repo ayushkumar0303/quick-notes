90582354bfab5fa4fe95531dea52b18b40d20405
Ayush Kumar <ayushk8295@gmail.com>
2025-08-02 15:14:19 +0530

    create basic react project and setup user data model in backend

---
+++ server/src/models/user.models.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("Users", UserSchema);
+
+export default User;
